e6aa45710eef567e7cdaade788448e9ba4473a80
Precious <Preshomondi@gmail.com>
2024-11-10 19:34:33 +0300

    Add files and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react'
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import BookMove from './pages/BookMove';
+import MyBookings from './pages/MyBookings';
+import Payment from './pages/Payment';
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/book-move" element={<BookMove />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/payment" element={<Payment />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
