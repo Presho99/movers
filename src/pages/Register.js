e0d5c664945e79b320d98cf14db5b4464b1973e3
Precious <Preshomondi@gmail.com>
2024-11-10 19:45:51 +0300

    Create registering component

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,40 @@
-import React from 'react'
+// src/pages/Register.js
+import React, { useState } from 'react';
 
 function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  const handleRegister = () => {
+    // Logic for registering the user
+    console.log('Registering:', name, email);
+  };
+
   return (
-    <div>Register</div>
-  )
+    <div className="register">
+      <h2>Register</h2>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
