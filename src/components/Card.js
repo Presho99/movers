306134bdf6692616b6aa250795b45ebde3b975f3
Precious <Preshomondi@gmail.com>
2024-11-11 13:00:56 +0300

    Style reusable card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,14 @@
 // src/components/Card.js
 import React from 'react';
+import './Card.css'; 
 
-function Card({ title, description }) {
+function Card({ title, children }) {
   return (
     <div className="card">
-      <h3>{title}</h3>
-      <p>{description}</p>
+      <h2>{title}</h2>
+      <div className="card-content">
+        {children}
+      </div>
     </div>
   );
 }
