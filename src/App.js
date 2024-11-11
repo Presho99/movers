344a7690ca41270f6ea375d130949550eae7e069
Precious <Preshomondi@gmail.com>
2024-11-11 13:03:29 +0300

    Add providers

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { AuthContext } from './context/AuthContext';
+import Navbar from './components/Navbar'; // Import Navbar
+import LandingPage from './pages/LandingPage';
 import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Profile from './pages/Profile';
 import BookMove from './pages/BookMove';
-import MyBookings from './pages/MyBookings';
-import Payment from './pages/Payment';
+import MyBooking from './pages/MyBookings';
 
 function App() {
+  const { isAuthenticated } = useContext(AuthContext);
+
   return (
     <Router>
-      <Navbar />
+      {isAuthenticated && <Navbar />} {/* Show Navbar only if authenticated */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/book-move" element={<BookMove />} />
-        <Route path="/my-bookings" element={<MyBookings />} />
-        <Route path="/payment" element={<Payment />} />
+        <Route
+          path="/"
+          element={
+            isAuthenticated ? <Navigate to="/home" /> : <LandingPage />
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            isAuthenticated ? <Home /> : <Navigate to="/" />
+          }
+        />
+                <Route
+          path="/book-move"
+          element={
+            isAuthenticated ? <BookMove /> : <Navigate to="/" />
+          }
+        />
+                        <Route
+          path="/my-bookings"
+          element={
+            isAuthenticated ? <MyBooking /> : <Navigate to="/" />
+          }
+        />
+
       </Routes>
     </Router>
   );
--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import AuthProvider from './context/AuthContext';
+import { BookingProvider } from './context/BookingContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <BookingProvider>
+        <App/>
+      </BookingProvider>
+
+    </AuthProvider>
+    
   </React.StrictMode>
 );
 
