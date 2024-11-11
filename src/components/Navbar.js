52266d6da1b9456d8ef90c2bafde63d6b2e28df6
Precious <Preshomondi@gmail.com>
2024-11-11 13:01:39 +0300

    Navbar routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,20 @@
 // src/components/Navbar.js
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 function Navbar() {
+  const { isAuthenticated } = useContext(AuthContext);
+
   return (
     <nav className="navbar">
-      <ul>
+      <ul className="nav-list">
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/register">Register</Link></li>
-        <li><Link to="/profile">Profile</Link></li>
         <li><Link to="/book-move">Book Move</Link></li>
         <li><Link to="/my-bookings">My Bookings</Link></li>
+        {isAuthenticated && (
+          <li className="profile-link"><Link to="/profile">Profile</Link></li>
+        )}
       </ul>
     </nav>
   );
